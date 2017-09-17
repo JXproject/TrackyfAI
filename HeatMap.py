0c4896f62747143612fc695a3eef833cd866eed8
Jack Xu <jack19980118@gmail.com>
2017-09-17 00:45:25 -0400

    Heap Map Finished, Dynamically schematica

--- HeatMap.py
+++ HeatMap.py
@@ -1,17 +1,26 @@
 import numpy as np
 import cv2
+import random
+import math
+import colorsys
 
 # ==================================FUNCTIONS================================
-#HeatMapAssign
-def Heat_Map_Generate( DataMat_, width_, height_):
-   HeatMap_img = np.zeros((len(HA[0]), len(HA), 3), dtype=np.uint8)
-   xPos, yPos = 0, 0
-   while xPos < len(DataMat_): #Loop through rows
-       while yPos < len(DataMat_[0]): #Loop through collumns
+#Heat Map Color  0 blue <-> 1 red
+def HeatMapClr(weight_):
+    return colorsys.hsv_to_rgb((weight_)*2.0/3.0, math.sqrt(weight_), 1)
 
-           HeatMap_img.itemset((yPos, xPos, 0), 255-int(DataMat_[xPos][yPos]*255)) #Set B to 255
-           HeatMap_img.itemset((yPos, xPos, 1), 255) #Set G to 255
-           HeatMap_img.itemset((yPos, xPos, 2), 255) #Set R to 255
+#HeatMapAssign
+def Heat_Map_Generate(DataMat_, width_, height_):
+   Amount_x = len(DataMat_[0])
+   Amount_y = len(DataMat_)
+   HeatMap_img = np.zeros((Amount_y, Amount_x, 3), dtype=np.uint8)
+   xPos, yPos = 0, 0
+   while xPos < Amount_x : #Loop through rows
+       while yPos < Amount_y : #Loop through collumns
+           HeatMap_Clr = HeatMapClr(DataMat_[yPos][xPos])
+           HeatMap_img.itemset((yPos, xPos, 0), HeatMap_Clr[0]*255) #Set B to 255
+           HeatMap_img.itemset((yPos, xPos, 1), HeatMap_Clr[1]*255) #Set G to 255
+           HeatMap_img.itemset((yPos, xPos, 2), HeatMap_Clr[2]*255) #Set R to 255
            yPos = yPos + 1 #Increment Y position by 1
 
        yPos = 0
@@ -19,18 +28,29 @@ def Heat_Map_Generate( DataMat_, width_, height_):
    return cv2.resize(HeatMap_img, (int(width_), int(height_)));
 
 # Update Heap Mat Accumulate Data
-def Heat_Map_Data_Mat_Update (DataMat_, x_, y_, weight):
-    if (y_<(len(HA[0])) and x_<(len(HA))) :
-        Prev_Value = DataMat_[y_][x_]
-        Prev_Value += weight
-        if (Prev_Value > 1):
-            DataMat_[y_][x_] = 1
-        elif (Prev_Value < 0):
-            DataMat_[y_][x_] = 0
-        else :
-            DataMat_[y_][x_] = Prev_Value
-    #CODE
-    return HeatMat_
+def Heat_Map_Data_Mat_Update (DataMat_, x_, y_, Screen_Width_, Screen_Height_, weight):
+    if(x_<=Scene_Width and y_<=Scene_Height and x_>=0 and y_>=0):
+        Unit_x = Screen_Width_/(len(DataMat_[0])-1)
+        Unit_y = Screen_Height_/(len(DataMat_)-1)
+        index_x_Centre = round(x_/Unit_x)
+        index_y_Centre = round(y_/Unit_y)
+        CentreValue = DataMat_[index_y_Centre][index_x_Centre]
+        #Will bring Up surrounding values
+        for i in range(0,1):
+            index_x = index_x_Centre + i
+            for j in range(0,1):
+                index_y = index_y_Centre + j
+                factor = 1 - (abs(i)+abs(j))/3 #Distance Factor
+                if (index_y<len(DataMat_) and index_x<len(DataMat_[0]) and index_x>=0 and index_y>=0) :
+                    Prev_Value = CentreValue + weight*factor
+                    if (Prev_Value > 1):
+                        DataMat_[index_y][index_x] = 1
+                    elif (Prev_Value < 0):
+                        DataMat_[index_y][index_x] = 0
+                    else :
+                        DataMat_[index_y][index_x] = Prev_Value
+        #CODE
+    return DataMat_
 # ==================================INITIALIZATION================================
 #Predefine Video Source
 cap = cv2.VideoCapture('VIRAT_S_000002.mp4')
@@ -46,14 +66,8 @@ Scene_Width = -1.0
 # kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
 # fgbg = cv2.createBackgroundSubtractorMOG2()
 
-# Accumulative Heap Mat Data 0-1
-HA = np.array([[1, 0.762, 0.954, 0.000, 0.835, 1],
-               [0.000, 0.000, 0.000, 0.000, 0.000, 0.000],
-               [0.000, 0.000, 0.000, 0.000, 0.000, 0.000],
-               [0.000, 0.000, 0.000, 0.000, 0.000, 0.000],
-               [0.000, 0.000, 0.000, 0.000, 0.000, 0.000],
-               [0.000, 0.000, 0.000, 0.000, 0.000, 0.000],
-               [1, 0.682, 0.755, 0.708, 0.643, 1]])
+# Accumulative Heat Mat Data 0-1 | [0]x-resolution [1]y-resolution
+Accumulative_Heat_Mat = np.zeros((5,5))
 
 # ==================================BODY================================
 while(cap.isOpened()):
@@ -74,7 +88,8 @@ while(cap.isOpened()):
     # cap_resized_gray_bgSub = cv2.morphologyEx(cap_gray_bgSub, cv2.MORPH_OPEN, kernel)
 
     # Heat Map
-    HeatMap_img_resized = Heat_Map_Generate(HA, Scene_Width, Scene_Height)
+    Accumulative_Heat_Mat = Heat_Map_Data_Mat_Update(Accumulative_Heat_Mat, Scene_Width*random.uniform(0,1) ,Scene_Height*random.uniform(0,1), Scene_Width ,Scene_Height ,0.1);
+    HeatMap_img_resized = Heat_Map_Generate(Accumulative_Heat_Mat, Scene_Width, Scene_Height)
 
     # Backgroud Subtractor
     # ret,thresh = cv2.threshold(cap_gray_bgSub_resized,127,255,0)
@@ -87,7 +102,8 @@ while(cap.isOpened()):
     cv2.putText(cap_resized,'Frame @ {}'.format(Frame_Counter),(10,25), font, 1, (255,255,255), 2)
 
     # Scene Blending
-    Result_Scene = cv2.addWeighted(cap_resized,1,HeatMap_img_resized,0.3,0)
+    Result_Scene = cv2.addWeighted(cap_resized,1,HeatMap_img_resized,1,0)
+    # Result_Scene = HeatMap_img_resized
     cv2.imshow('frame',Result_Scene)
 
     # Frame Incrementing
